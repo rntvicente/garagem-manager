86145573cbc7d68a55c3eda331e7d0d768fec788
renato.vicente <rntvicente@gmail.com>
2019-01-27 21:53:30 -0200

    #GET Caso de Test Consumers

--- test/integration/consumer/consumer-test.js
+++ test/integration/consumer/consumer-test.js
@@ -9,7 +9,7 @@ const { consumer } = require('../../fixtures');
 
 describe('#POST Caso de Test Consumers', () => {
   describe('Casos de sucesso', () => {
-    it('Deve retornar 202 quando chamada a route /post', (done) => {
+    it('Deve retornar 202 quando chamado a rota', (done) => {
       const input = consumer.dbModel();
 
       const stub = sinon.stub(model, 'insertOne')
@@ -66,6 +66,26 @@ describe('#POST Caso de Test Consumers', () => {
     it('Deve retornar 400 quando mobile for invalido', (done) => {
       const input = {
         mobile: 'batata'
+      };
+
+      const body = {
+        message: 'Failed operation.'
+      };
+
+      request(app)
+        .post('/consumers')
+        .send(input)
+        .expect(httpStatusCode.badRequest)
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.deepEqual(res.body, body);
+          done();
+        });
+    });
+
+    it('Deve retornar 400 quando informado mobile sem DDI', (done) => {
+      const input = {
+        mobile: '982247777'
       };
 
       const body = {
@@ -153,6 +173,47 @@ describe('#POST Caso de Test Consumers', () => {
         .post('/consumers')
         .send(input)
         .expect(httpStatusCode.internalServerError)
+        .end((err) => {
+          assert.isNull(err);
+          stub.restore();
+          done();
+        });
+    });
+  });
+});
+
+describe('#GET Caso de Test Consumers', () => {
+  describe('Casos de Sucesso', () => {
+    it('Deve retornar 200 quando encontrar mobile 5511982247171', (done) => {
+      const input = consumer.dbModel();
+
+      const stub = sinon.stub(model, 'findOne')
+        .callsFake((arg1, callback) => callback(null, input));
+
+      request(app)
+        .get(`/consumers/mobile/${input.mobile}`)
+        .send(input)
+        .expect(httpStatusCode.ok)
+        .end((err, res) => {
+          assert.isNull(err);
+          assert.deepEqual(res.body, input);
+          stub.restore();
+          done();
+        });
+    });
+  });
+
+  describe('Casos de Falhas', () => {
+    it('Deve retonar 400 quando informado mobile sem DDD', (done) => {
+      const input = consumer.dbModel({ mobile: '982247184' });
+
+      const stub = sinon.stub(model, 'findOne')
+        .callsFake((arg1, callback) => callback(null, input));
+
+      request(app)
+        .get(`/consumers/mobile/${input.mobile}`)
+        .send(input)
+        .expect(httpStatusCode.badRequest)
         .end((err) => {
           assert.isNull(err);
           stub.restore();
