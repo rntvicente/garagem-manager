2aee688684f08423ecc671169e0e9534c6a6f509
renato.vicente <rntvicente@gmail.com>
2019-01-28 22:55:26 -0200

    Adição de nova route para Carros

---
+++ test/integration/car/car-test.js
@@ -0,0 +1,44 @@
+const { assert } = require('chai');
+const request = require('supertest');
+
+const app = require('../../../lib/server');
+const { httpStatusCode } = require('../../../lib/commons/utils');
+
+describe('#POST Casos de Test Car', () => {
+  describe('Casos de Sucesso', () => {
+    it('Deve retornar 202 quando quando chamado route.', (done) => {
+      request(app)
+        .post('/car/board/FFF1234')
+        .send()
+        .expect(httpStatusCode.accepted)
+        .end((err) => {
+          assert.isNull(err);
+          done();
+        });
+    });
+  });
+
+  describe('Casos de Falhas', () => {
+    it('Deve retonar 404 quando não informando route correto.', (done) => {
+      request(app)
+        .post('/car')
+        .send()
+        .expect(httpStatusCode.notFound)
+        .end((err) => {
+          assert.isNull(err);
+          done();
+        });
+    });
+
+    it('Deve retonar 404 quando não tiver placa informada.', (done) => {
+      request(app)
+        .post('/car/board/')
+        .send()
+        .expect(httpStatusCode.notFound)
+        .end((err) => {
+          assert.isNull(err);
+          done();
+        });
+    });
+  });
+});
