04240be9381704ac9b175cd4e8a418f0e7843279
renato.vicente <rntvicente@gmail.com>
2019-04-16 08:45:14 -0300

    alteração de var

--- test/global.js
+++ test/global.js
@@ -4,7 +4,7 @@ const database = require('../lib/commons/database');
 const config = require('../lib/commons/conf');
 
 before((done) => {
-  database.connect(config.get('MONGO_TEST'), (err) => {
+  database.connect(config.get('MONGO_URL'), (err) => {
     if (err) {
       chalk.red('Shutdown the application because an error occurred when connecting to database');
       process.exit(1);
